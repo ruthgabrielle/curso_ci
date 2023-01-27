dae9d6e22dc89f156816ac32e543f3390c8e9ba3
ruthgabrielle <ruthfgms@gmail.com>
2023-01-27 12:15:56 -0300

    space

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host="+os.Getenv("HOST")+"user="+os.Getenv("USER")+"password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+"port="+os.Getenv("PORT")+" sslmode=disable"
+	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
